a2ab2bd4acf934163d6717918d3231544f8e4a6f
Barys <barys.radzko@protonmail.com>
2024-12-22 13:21:23 +0100

    Added player texture rotation

--- include/player.h
+++ include/player.h
@@ -9,17 +9,14 @@ public:
   void Draw();
   void Update();
   void Move(Vector2 direction);
-  // void MoveRight();
-  // void MoveLeft();
-  // void MoveUp();
-  // void MoveDown();
+  void Rotate(float degree);
   void Attack();
 
 private:
-  Vector2 SumVector2(Vector2& a, Vector2& b);
-  Texture2D image;
   Vector2 position;
   Vector2 direction;
+  Texture2D image;
+  float rotation;
   int speed;
   int health;
 
--- src/player.cpp
+++ src/player.cpp
@@ -1,6 +1,5 @@
 #include "../include/player.h"
 #include <raymath.h>
-#include <iostream>
 
 
 Player::Player()
@@ -8,7 +7,8 @@ Player::Player()
   image = LoadTexture("assets/sprites/player/pTest.png");
   position.x = (GetScreenWidth() - image.width)/2;
   position.y = (GetScreenHeight() - image.width)/2;
-  speed = 10;
+  rotation = 0;
+  speed = 5;
 }
 
 Player::~Player()
@@ -18,7 +18,17 @@ Player::~Player()
 
 void Player::Draw()
 {
-  DrawTextureV(image, position, WHITE);
+  // Define the source rectangle (entire texture) *Consider later usage of static_cast<float>
+  Rectangle sRect = {0, 0, (float)image.width, (float)image.height};
+
+  // Define the destination rectangle (where the texture will be drawn)
+  Rectangle dRect = {position.x, position.y, (float)image.width, (float)image.height};
+
+  // Set the origin of rotation (center of the texture)
+  Vector2 origin = { image.width / 2.0f, image.height / 2.0f };
+
+  // Draw the texture with rotation
+  DrawTexturePro(image, sRect, dRect, origin, rotation, WHITE);
 }
 
 
@@ -28,10 +38,12 @@ void Player::Move(Vector2 direction)
   this->direction.x = direction.x;
   this->direction.y = direction.y;
 
-  // Normalizing Vector two, so the movement dioganally is not faster
+
+  // Normalizing Vector2: Vector2 length is needed
   float magnitude = sqrt((this->direction.x * this->direction.x) +
                          (this->direction.y * this->direction.y));
 
+  // Normalizing Vector2: so the movement dioganally is not faster
   if (magnitude != 0)
   {
     this->direction.x = this->direction.x / magnitude;
@@ -41,6 +53,11 @@ void Player::Move(Vector2 direction)
   // Moving player
   position.x += this->direction.x * speed;
   position.y += this->direction.y * speed;
+
+  if (magnitude != 0)
+    {
+      rotation = atan2(this->direction.y, this->direction.x) * RAD2DEG + 90; // Convert radians to degrees
+    }
 
 }
 
